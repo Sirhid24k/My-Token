30231943d96bd9eec0c75db05cdc304de89b5f7c
Sirhid24k <abdulateefsaheed7@gmail.com>
2025-07-16 00:47:56 +0100

    Create deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+const hre = require('hardhat');
+
+async function main() {
+  // Get the deployer's account
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log('Deploying contracts with the account:', deployer.address);
+  console.log('Account balance:', (await deployer.getBalance()).toString());
+
+  // Get the ContractFactory for MyToken
+  const Token = await hre.ethers.getContractFactory('MyToken');
+
+  // Deploy the contract
+  const myToken = await Token.deploy();
+
+  // Wait for the deployment to be confirmed on the blockchain
+  await myToken.waitForDeployment();
+
+  console.log('MyToken deployed to address:', myToken.target);
+}
+
+// Pattern to handle errors and exit process
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
