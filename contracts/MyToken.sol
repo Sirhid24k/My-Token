da08fc428e8e8b00f54246ecec3f64a2298af5f8
Sirhid24k <abdulateefsaheed7@gmail.com>
2025-07-16 00:05:11 +0100

    Defining contract and config file

---
+++ contracts/MyToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    address public owner;
+
+    constructor() ERC20("My Awesome Token", "MAT") {
+        // Set Owner of contract
+        owner = msg.sender;
+        // Mint an initial supply of 1,000,000 tokens
+        _mint(msg.sender, 1_000_000 * (10 ** decimals()));
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config(); // Loading environment variables from .env
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: '0.8.28', // Solidity version, e.g. "0.8.20"
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
